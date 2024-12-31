874a9e2797b9e12dec3776b229b6d40e0fc84bd4
dokee <dokee.39@gmail.com>
2024-12-31 12:00:44 +0800

    Synced on Tue 31 Dec 12:00:44 CST 2024

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local lspconfig = require "lspconfig"
 
 -- EXAMPLE
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-local servers = { "html", "cssls", "clangd", "cmake", "bashls", "matlab_ls", "taplo", "astro"}
+local servers = { "html", "cssls", "clangd", "cmake", "bashls", "matlab_ls", "taplo", "astro", "ts_ls", "htmx"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
